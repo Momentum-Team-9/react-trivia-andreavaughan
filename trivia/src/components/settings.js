47bbbad20c286bc365b7cb8eaf2dff4f5fb27d87
andreavaughan <amvaughan16@gmail.com>
2021-09-08 09:53:17 -0400

    added category rendering with axios

--- trivia/src/components/settings.js
+++ trivia/src/components/settings.js
@@ -1,44 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const Settings = () => {
+    const [ categories, setCategories ] = useState([])
+    
+    useEffect(() => {
+        axios
+            .get(
+                `https://opentdb.com/api_category.php`
+            )
+            .then((response)  => {
+                setCategories(response.data.trivia_categories.map((category) => [category.name, category.id]))
+            })
+    }, [])
+
+    console.log(categories)
 
     return (
         <>
-            <h3>Select a difficulty and category. Once selected, click 'start' to begin.</h3>
-            <div>
-                <h4>Difficulty</h4>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="easy-radio" />
-                    <label className="form-check-label" for="easy-radio">
-                        Easy
-                    </label>
+            <form>
+                <h3>Select a difficulty and category. Once selected, click 'start' to begin.</h3>
+                <div>
+                    <h4>Difficulty</h4>
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" name="difficulty-select" id="easy-radio" />
+                        <label className="form-check-label" htmlFor="easy-radio">
+                            Easy
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" name="difficulty-select" id="medium-radio" />
+                        <label className="form-check-label" htmlFor="medium-radio">
+                            Medium
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" name="difficulty-select" id="hard-radio" />
+                        <label className="form-check-label" htmlFor="hard-radio">
+                            Hard
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" name="difficulty-select" id="random-radio" />
+                        <label className="form-check-label" htmlFor="random-radio">
+                            Random
+                        </label>
+                    </div>
+                    <h4>Category</h4>
+                    <select className="form-select categories" aria-label="Category select">
+                            <option className="category" id="">Any Category</option>
+                        { categories.map((category) => (
+                            <option className="category" id={category[1]}>{category[0]}</option>
+                        ))}
+                    </select>
                 </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="medium-radio" />
-                    <label className="form-check-label" for="medium-radio">
-                        Medium
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="hard-radio" />
-                    <label className="form-check-label" for="hard-radio">
-                        Hard
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="random-radio" />
-                    <label className="form-check-label" for="random-radio">
-                        Random
-                    </label>
-                </div>
-                <h4>Category</h4>
-                <select className="form-select" aria-label="Category select">
-                    <option>Select dropdown</option>
-                    <option>With options</option>
-                    <option>With more</option>
-                </select>
-            </div>
-            <button type="button" className="btn btn-danger btn-lg">Start</button>
+                <button type="submit" className="btn btn-danger btn-lg">Start</button>
+            </form>
         </>
     )
 }
