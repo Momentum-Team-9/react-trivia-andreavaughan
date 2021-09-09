37a9bdd390919dd677db12e74674e3de9b2e6832
andreavaughan <amvaughan16@gmail.com>
2021-09-09 11:08:07 -0400

    added question component render

--- trivia/src/components/questions.js
+++ trivia/src/components/questions.js
@@ -4,34 +4,19 @@ export const Questions = ({ questions }) => {
     console.log(questions)
 
     return (
-        <div>
-            <form>
-                <h3>Question</h3>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="question-radio-1" />
-                    <label className="form-check-label" htmlFor="question-radio-1">
-                        Answer 1
-                    </label>
+        <>
+            {questions.map((question, idx) => (
+                <div className="card" key={idx}>
+                    <div className="card-body">
+                        <h4 className="card-title">{question.question}</h4>
+                        <div className="d-grid gap-2">
+                            <button className="btn btn-primary" type="button">{question.correct_answer}</button>
+                            <button className="btn btn-primary" type="button">{question.incorrect_answers}</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="question-radio-2" />
-                    <label className="form-check-label" htmlFor="question-radio-2">
-                        Answer 2
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="question-radio-3" />
-                    <label className="form-check-label" htmlFor="question-radio-3">
-                        Answer 3 
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name="difficulty-select" id="question-radio-4" />
-                    <label className="form-check-label" htmlFor="question-radio-4">
-                        Answer 4 
-                    </label>
-                </div>
-            </form>
-        </div>
+                )
+            )}
+        </>
     )
 }
--- trivia/src/index.js
+++ trivia/src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import './styles/index.css'
 import ReactDom from 'react-dom'
 import { App } from './components/App.js'
 
