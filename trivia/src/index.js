16e041662bfd9d2e030204aa038a67defbf947f3
andreavaughan <amvaughan16@gmail.com>
2021-09-10 14:13:06 -0400

    changes for netlify deployment

--- trivia/src/index.js
+++ trivia/src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/index.css'
 import ReactDom from 'react-dom'
-import { App } from './components/App'
+import { App } from './App'
 
 ReactDom.render (
     <App />,
