5e7601122b7262e7ba2b5fa96eaa64cea7ec5ad3
andreavaughan <amvaughan16@gmail.com>
2021-09-13 17:25:19 -0400

    deleted endgame card

--- trivia/src/components/QuestionsList.js
+++ trivia/src/components/QuestionsList.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Question } from './Question'
 import { Score } from './Score'
-import { EndgameCard } from './EndgameCard'
 import { QuestionCounter } from './QuestionCounter'
 import '../styles/questions-list.css'
 
@@ -18,26 +17,32 @@ export const QuestionsList = ({ questions, setQuestions }) => {
     return (
         <>
             { questions[currentIndex] !== undefined ?
-                <Question 
-                    question={questions[currentIndex]} 
-                    key={questions[currentIndex].id} 
-                    isCorrect={isCorrect}
-                    setIsCorrect={setIsCorrect}
-                    currentIndex={currentIndex}
-                    setCurrentIndex={setCurrentIndex}
-                /> :
-                <EndgameCard score={score} setQuestions={setQuestions}/>
+                <>
+                    <Question 
+                        question={questions[currentIndex]} 
+                        key={questions[currentIndex].id} 
+                        isCorrect={isCorrect}
+                        setIsCorrect={setIsCorrect}
+                        currentIndex={currentIndex}
+                        setCurrentIndex={setCurrentIndex}
+                    />
+                    <div className="question-nums">
+                        <QuestionCounter questions={questions} currentIndex={currentIndex}/>
+                        <Score isCorrect={isCorrect} score={score} setScore={setScore}/>
+                    </div>
+                </> :
+                <>
+                    <div className="endgame">
+                        <h3>You got {score} right!</h3>
+                        <button 
+                        className="btn btn-success" 
+                        onClick={() => (setQuestions([]))}
+                        >
+                            Play Again
+                        </button>
+                    </div>
+                </>
             }
-            <div className="question-nums">
-                {questions[currentIndex] !== undefined ? 
-                    <QuestionCounter questions={questions} currentIndex={currentIndex}/> :
-                    <div></div> 
-                }
-                { questions[currentIndex] !== undefined ? 
-                    <Score isCorrect={isCorrect} score={score} setScore={setScore}/> :
-                    <div></div>
-                }
-            </div>
         </>
     )
 }
